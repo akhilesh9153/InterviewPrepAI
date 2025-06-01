fe4e3d7ec00b31bb223650cfccf1dcea2ecba02d
akhilesh9153 <115997933+akhilesh9153@users.noreply.github.com>
2025-06-01 18:48:59 +0530

    Update App.jsx
    sign up url change

--- frontend/interview-prep-ai/src/App.jsx
+++ frontend/interview-prep-ai/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import Login from "./pages/Auth/Login";
-import SignUp from "./pages/Auth/signup";
+import SignUp from "./pages/Auth/SignUp";
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Home/Dashboard";
 import InterviewPrep from "./pages/InterviewPrep/InterviewPrep";
